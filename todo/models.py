f834aca683b10af1fcc66acc2f29fbd9dbe996cd
Ryosuke Asano <s1F102400011@iniad.org>
2025-06-26 13:33:20 +0900

    Taskモデルを追加

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    due_at = models.DateTimeField(null=True, blank=True)
