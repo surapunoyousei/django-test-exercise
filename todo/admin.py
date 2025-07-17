3fe931d0e7987280da257a52600f923b3450f438
1F10240128 <s1f102401281@iniad.org>
2025-07-17 14:22:25 +0900

    Enhanced the control page

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_closed', 'created_at')
+    search_fields = ('title',)
+    list_filter = ('is_closed',)
+
+admin.site.register(Task, TaskAdmin)
