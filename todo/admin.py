a930cb4053beeb4c64ab2790632472e6f5742f3c
1F10240128 <s1f102401281@iniad.org>
2025-07-17 15:38:20 +0900

    Reset admin.py and add an element on models.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-from .models import Task
-
-
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'completed', 'posted_at', 'priority')
-    search_fields = ('title', )
-    list_filter = ('completed', 'priority')
-
-
-admin.site.register(Task, TaskAdmin)
