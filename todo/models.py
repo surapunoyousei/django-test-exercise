ab22f799cf269eb6665a1b64d633ff645c177447
taharu-bless77 <s1f102401742@iniad.org>
2025-07-22 13:44:09 +0900

    変更

--- todo/models.py
+++ todo/models.py
@@ -4,10 +4,16 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', '低'),
+        ('medium', '中'),
+        ('high', '高'),
+    ]
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(auto_now_add=True)
     due_at = models.DateTimeField(null=True, blank=True)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
 
     def is_overdue(self, dt):
         if self.due_at is None:
